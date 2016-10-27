266bf955ce2a697b63363120bf814a2b86de6c52
klein <mhxy199288@vip.qq.com>
2016-10-27 19:20:50 +0800

    Container With Most Water

---
+++ src/main/java/org/blue/learning/leetcode/day10/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package org.blue.learning.leetcode.day10;
+
+import java.util.BitSet;
+
+/**
+ * @author leiyi
+ * @date 2016-10-27 18:52
+ * @summary:LongestCommonPrefix
+ */
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        BitSet bitSet = null;
+        int index = 0;
+        StringBuilder commomPrefix = new StringBuilder();
+        return null;
+    }
+}
