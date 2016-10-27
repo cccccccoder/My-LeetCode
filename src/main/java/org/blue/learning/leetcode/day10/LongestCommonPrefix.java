64f30105ef9dea6cb3ecb473af0fd9e35d2506ee
klein <mhxy199288@vip.qq.com>
2016-10-27 19:32:47 +0800

    Container With Most Water

--- src/main/java/org/blue/learning/leetcode/day10/LongestCommonPrefix.java
+++ src/main/java/org/blue/learning/leetcode/day10/LongestCommonPrefix.java
@@ -1,6 +1,5 @@
 package org.blue.learning.leetcode.day10;
 
-import java.util.BitSet;
 
 /**
  * @author leiyi
@@ -10,13 +9,37 @@ import java.util.BitSet;
 public class LongestCommonPrefix {
 
     public static void main(String[] args) {
-
+        System.out.println(longestCommonPrefix(new String[] {"aaa", "bbb", "ccc"}));
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        BitSet bitSet = null;
+        if (strs.length == 0) {
+            return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
+        }
         int index = 0;
-        StringBuilder commomPrefix = new StringBuilder();
-        return null;
+        int xorResult;
+        StringBuilder result = new StringBuilder();
+        while(true) {
+            xorResult = -1;
+            for (String s : strs) {
+                if (index < s.length()) {
+                    if (xorResult == -1) {
+                        xorResult = s.charAt(index);
+                    } else {
+                        xorResult = xorResult ^ s.charAt(index);
+                    }
+                } else {
+                    return result.toString();
+                }
+            }
+            if (xorResult == 0) {
+                return result.toString();
+            }
+            result.append(strs[0].charAt(index));
+            index ++;
+        }
     }
 }
