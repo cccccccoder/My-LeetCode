b13170b5ce435f42fe7eec630b6e268f967b36a4
klein <mhxy199288@vip.qq.com>
2016-11-03 19:21:59 +0800

    day22 Remove Element

--- src/main/java/org/blue/learning/leetcode/day22/RemoveElement.java
+++ src/main/java/org/blue/learning/leetcode/day22/RemoveElement.java
@@ -8,15 +8,22 @@ package org.blue.learning.leetcode.day22;
 public class RemoveElement {
 
     public static void main(String[] args) {
-
+        int[] array = new int[] {3,2,2,3,2,4,5};
+        System.out.println(removeElement(array, 22));
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
 
-    public int removeElement(int[] nums, int val) {
+    public static int removeElement(int[] nums, int val) {
 
         int count = 0;
         for (int i = 0; i < nums.length; i ++) {
-
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count ++;
+            }
         }
-        return 0;
+        return count;
     }
 }
