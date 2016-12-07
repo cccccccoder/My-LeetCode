8cf9468cf037f0a1b6a3278fd6de0591466e48f8
klein <mhxy199288@vip.qq.com>
2016-12-07 18:37:16 +0800

    day68 MinimumWindowSubstring

--- src/main/java/org/blue/learning/leetcode/day68/MinimumWindowSubstring.java
+++ src/main/java/org/blue/learning/leetcode/day68/MinimumWindowSubstring.java
@@ -1,5 +1,7 @@
 package org.blue.learning.leetcode.day68;
 
+import java.util.HashMap;
+
 /**
  * @author leiyi
  * @date 2016-12-05 18:05
@@ -25,10 +27,43 @@ public class MinimumWindowSubstring {
      * @return
      */
     public static String minWindow(String s, String t) {
-        int len = t.length();
-        for (int i = len; i < s.length(); i ++) {
+        HashMap<Character, Integer> hasFound = new HashMap<>();
+        HashMap<Character, Integer> needToFind = new HashMap<>();
 
+        for (int i = 0; i < t.length(); i++) {
+             hasFound.put(t.charAt(i), 0);
+             if (needToFind.containsKey(t.charAt(i))) {
+                needToFind.put(t.charAt(i), needToFind.get(t.charAt(i)) + 1);
+             } else {
+                needToFind.put(t.charAt(i), 1);
+             }
         }
-        return null;
+        int begin = 0;
+        int minWindowSize = s.length();
+        String retString = "";
+        int count = 0;
+        for (int end = 0; end < s.length(); end++) {
+             Character end_c = s.charAt(end);
+             if (needToFind.containsKey(end_c)) {
+                 hasFound.put(end_c, hasFound.get(end_c) + 1);
+                 if (hasFound.get(end_c) <= needToFind.get(end_c)) {
+                            count++;
+                 }
+                 if (count == t.length()) {
+                     while ((!needToFind.containsKey(s.charAt(begin))) || (hasFound.get(s.charAt(begin)) > needToFind.get(s.charAt(begin)))) {
+                             if (needToFind.containsKey(s.charAt(begin))) {
+                                 hasFound.put(s.charAt(begin), hasFound.get(s.charAt(begin)) - 1);
+                             }
+                            begin++;
+                     }
+                     if ((end - begin + 1) <= minWindowSize) {
+                         minWindowSize = end - begin + 1;
+                         retString = s.substring(begin, end + 1);
+                     }
+                 }
+             }
+        }
+
+        return retString;
     }
 }
